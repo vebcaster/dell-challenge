4b2b1df1a28561dff37798446d0a9fa590f0721a
Andrei_Ursta <andrei_ursta@dell.com>
2019-02-05 19:05:06 +0200

    initial commit

---
+++ DellChallenge/DellChallenge.B/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DellChallenge.B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given the classes and interface below, please constructor the proper hierarchy.
+        }
+    }
+
+    public interface ISpecies
+    {
+        void Eat();
+        void Drink();
+        void Fly();
+        void Swim();
+    }
+
+    public class Species
+    {
+        public virtual void GetSpecies()
+        {
+            Console.WriteLine($"Echo who am I?");
+        }
+    }
+
+    public class Human : ISpecies
+    {
+        public void Drink()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swim()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Bird
+    {
+    }
+
+    public class Fish
+    {
+    }
+}
+
---
+++ DellChallenge/DellChallenge.D1.Api/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using DellChallenge.D1.Api.Dal;
+using DellChallenge.D1.Api.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DellChallenge.D1.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsService _productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductDto>> Get()
+        {
+            return Ok(_productsService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public ActionResult<ProductDto> Post([FromBody] NewProductDto newProduct)
+        {
+            var addedProduct = _productsService.Add(newProduct);
+            return Ok(addedProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+    }
+}
